0010bf2e76fd56d7e5f1815396fbad7aec5f02f0
gustavodiasag <gustavodias.aguiar1@gmail.com>
2023-08-08 15:06:21 -0300

    feat: file reading

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    // The `args` function provides an iterator over the arguments
+    // passed to the program and `collect` turns that iterator
+    // into a collection.
+    let args: Vec<String> = env::args().collect();
+    
+    let query = &args[1];
+    let filename = &args[2];
+    // The `read_to_string` function takes a file and converts its
+    // content into a string, returning a result. To handle the Err
+    // variant, `expect` is used, throwing an error message.
+    let contents = fs::read_to_string(filename)
+        .expect("Something went wrong while reading the file.");
+
+    println!("{}", contents);
 }
